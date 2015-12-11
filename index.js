f594d8f1eae7c432c70fac1ea60d61739a21bee8
Jacob Blakely <jacob@codekirei.com>
2015-12-11 01:43:53 -0800

    improve defaultTemplate fn

--- index.js
+++ index.js
@@ -67,7 +67,6 @@ module.exports = class Jamb {
   * main() {
     const pages = yield this.pages(this._paths.pages)
     const posts = yield this.posts(this._paths.posts)
-    console.log(posts)
     const content = flatAr([pages, posts])
 
     const templateContent = groupByTemplate(content)
